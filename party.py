8ed60658c3b7fd98a99adce1851e6ce01ca21ec9
Kunal Katarya <kunal.rox@gmail.com>
2020-11-18 16:05:42 +0800

    cosmetic improvements

--- party.py
+++ party.py
@@ -7,7 +7,9 @@ import circuit
 import modprime
 
 def gen_coeffs(priv_value):
-  '''Generate random polynomial (degree = DEGREE) where constant=secret'''
+  '''
+  Generate random polynomial (degree = DEGREE) where constant=secret
+  '''
   if DEGREE < 1:
     log.write("[WARNING] Invalid degree value: %d" % DEGREE)
   log.debug("degree %d" % DEGREE)
@@ -17,7 +19,9 @@ def gen_coeffs(priv_value):
   return coeffs
 
 def create_share(coeffs: object, x: object) -> object:
-  ''' Retuns P(x) mod PRIME '''
+  '''
+  Retuns P(x) mod PRIME
+  '''
   evals = []
   for deg, co in enumerate(coeffs, start=0):
     evals.append(co*(x**deg))
@@ -27,7 +31,9 @@ def create_share(coeffs: object, x: object) -> object:
   return modprime.mod(res)
 
 def interpolate(received_values, final=False):
-  '''Perform lagrange interpolation to recover secret'''
+  '''
+  Perform lagrange interpolation to recover secret
+  '''
   upper_bound = DEGREE+2 if final else 2*DEGREE+2
   recomb = []
   for i in range(1, upper_bound):
@@ -44,18 +50,10 @@ def interpolate(received_values, final=False):
      circuit.function(circuit.PRIVATE_VALUES)))
   return modprime.mod(res)
 
-def output_gate(gate_inputs, final_gate, network):
-    if len(gate_inputs[final_gate]) != 1:
-      log.write("[WARNING] Incorrect number of inputs: %d" % len(gate_inputs[final_gate]))
-    final_value = gate_inputs[final_gate][0]
-    log.debug("Sending final value %d all parties" % (gate_inputs[final_gate][0]))
-    send_final_value(final_value, final_gate, network)
-    final_shares = receive_shares(final_gate, network)
-    log.write("Final shares %s" % final_shares)
-    final_result = interpolate(final_shares, final=True)
-
 def create_and_send_shares(src_gate, polynomial, network):
-    '''Create and send a share to each party based on a polynomial'''
+    '''
+    Create and send a share to each party based on a polynomial
+    '''
     sent_shares = []
     for dest_party in ALL_PARTIES:
       share = create_share(polynomial, dest_party)
@@ -65,19 +63,25 @@ def create_and_send_shares(src_gate, polynomial, network):
     log.write("Sent shares %s" % sent_shares)
 
 def receive_shares(src_gate, network):
-    '''Receive shares for the current gate from all parties'''
+    '''
+    Receive shares for the current gate from all parties
+    '''
     received_shares = []
     for party in ALL_PARTIES:
         received_shares.append(network.receive_share(party, src_gate))
     return received_shares
 
 def send_final_value(final_value, final_gate, network):
-    '''Share final output with all other parties'''
+    '''
+    Share final output with all other parties
+    '''
     for party in ALL_PARTIES:
         network.send_share(final_value, final_gate, party)
 
 def degree_reduction(needs_reduction_res, curr_gate, network):
-    '''Wrapper function to perform degree reduction'''
+    '''
+    Wrapper function to perform degree reduction
+    '''
     deg_red_coeffs = gen_coeffs(needs_reduction_res)
     log.write("Degree reduction coeffs %s" % deg_red_coeffs)
     create_and_send_shares(curr_gate, deg_red_coeffs, network)
@@ -87,7 +91,9 @@ def degree_reduction(needs_reduction_res, curr_gate, network):
     return res
 
 def add_gate(gate_inputs, curr_gate, next_gate, input_no):
-    '''Add two inputs, store result'''
+    '''
+    Add two inputs, store result
+    '''
     if (len(gate_inputs[curr_gate]) != 2):
       log.write("[WARNING] Incorrect number of inputs: %d" % len(gate_inputs[curr_gate]))
     op1,op2 = gate_inputs[curr_gate]
@@ -114,14 +120,32 @@ def mul_gate(gate_inputs, curr_gate, next_gate, input_no, network):
       gate_inputs[next_gate].append(res)
 
 def input_gate(gate_inputs, curr_gate, next_gate, input_no, network):
-    '''Receive a share for the current gate from the corresponding party'''
+    '''
+    Receive a share for the current gate from the corresponding party
+    '''
     share = network.receive_share(curr_gate, curr_gate)
     if input_no == 1:
       gate_inputs[next_gate] = [share]
     else:
       gate_inputs[next_gate].append(share)
 
+def output_gate(gate_inputs, final_gate, network):
+    '''
+    Perform shamir secret sharing for the final value of this party's circuit
+    '''
+    if len(gate_inputs[final_gate]) != 1:
+      log.write("[WARNING] Incorrect number of inputs: %d" % len(gate_inputs[final_gate]))
+    final_value = gate_inputs[final_gate][0]
+    log.debug("Sending final value %d all parties" % (gate_inputs[final_gate][0]))
+    send_final_value(final_value, final_gate, network)
+    final_shares = receive_shares(final_gate, network)
+    log.write("Final shares %s" % final_shares)
+    final_result = interpolate(final_shares, final=True)
+
 def bgw_protocol(party_no, priv_value, network):
+  '''
+  Wrapper function that implements the skeleton of the BGW protocol.
+  '''
   log.debug("Called bgw protocol!")
   log.debug("Private value: %d, party no %d" % (priv_value, party_no))
 
